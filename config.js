ab91274c53f3d5e7bc226777c463465d07bf01d0
daewoongBang <daewoong.bang@gmail.com>
2020-01-14 18:41:15 +0900

    config.js 수정

--- config.js
+++ config.js
@@ -1,19 +1,18 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://daewoong.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle:
-    'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Daewoong Dev Blog',
+  subtitle: 'Carpe diem!',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-155111945-1',
   useKatex: false,
   menu: [
     {
-      label: 'Articles',
+      label: 'Posts',
       path: '/'
     },
     {
@@ -36,18 +35,17 @@ module.exports = {
   author: {
     name: 'Daewoong Bang',
     photo: '/photo.jpg',
-    bio:
-      'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Carpe diem!',
     contacts: {
       email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
+      facebook: '',
+      telegram: '',
+      twitter: '',
+      github: 'daewoongBang',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: '',
+      instagram: '',
       line: '',
       gitlab: '',
       weibo: '',
