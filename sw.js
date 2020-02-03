d17e33739d0a58732c64d3523173fb0464777c2e
daewoongBang <daewoong.bang@gmail.com>
2020-02-03 12:38:46 +0900

    Updates

--- sw.js
+++ sw.js
@@ -36,7 +36,7 @@ self.__precacheManifest = [
     "url": "styles-117479406f6e61ad4793.js"
   },
   {
-    "url": "commons-9b5af6ebd205c2e78af4.js"
+    "url": "commons-2ba4ee1f2812036bcf36.js"
   },
   {
     "url": "netlify-identity-widget-8e6f024872c0d48ae324.js"
@@ -49,7 +49,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "3b34ee44efc2f41f2cce8dcf5a28e33d"
+    "revision": "5d499ca72f86a8a9e748c9a8000cc2cc"
   },
   {
     "url": "manifest.json",
